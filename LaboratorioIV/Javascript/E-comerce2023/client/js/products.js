43cc9b2c8ff72ae46ba93f3a3b747b2315fd4562
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2023-09-07 00:24:23 -0300

    subiendo js ecommerce segunda clase

---
+++ LaboratorioIV/Javascript/E-comerce2023/client/js/products.js
@@ -0,0 +1,45 @@
+const productos =  [
+    {
+        id : 1,
+        productName : "Camaro (Oferta Solo Team Hunt Code) ",
+        price:10,
+        quantity: 1,
+        img :"/client/media/camara.jpeg",
+    },
+    {
+        id : 2,
+        productName : "chevrolet",
+        price:9500,
+        quantity: 1,
+        img :"/client/media/chevrolet.jpeg",
+    },
+    {
+        id : 3,
+        productName : "Chebrolet Blue Soja ",
+        price:7500,
+        quantity: 1,
+        img :"/client/media/chevroletazul.jpeg",
+    },
+    {
+        id : 4,
+        productName : "Ford Orange ",
+        price:2800,
+        quantity: 1,
+        img :"/client/media/ford.jpeg",
+    },
+    {
+        id : 5,
+        productName : "Toyota XL",
+        price:3200,
+        quantity: 1,
+        img :"/client/media/Toyota.jpeg",
+    },
+    {
+        id : 6,
+        productName : "Oferta Automoviles ",
+        price:200,
+        quantity: 1,
+        img :"/client/media/oferta.jpeg",
+    }
+
+];	    
