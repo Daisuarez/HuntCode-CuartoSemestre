0844dfb59ebaf3ccb76e0b7d89b95757cc03dc0e
Mayra Medina <111921764+Maynh@users.noreply.github.com>
2023-10-08 22:27:05 -0300

    subiendo

---
+++ lección01/.idea/tipo.str.py
@@ -0,0 +1,12 @@
+import  math
+
+# Profundizando en el tipo String
+# Concatenación automatica en Python
+
+variable = ' Adiós'
+mensaje = 'Hola' ' alumnos,' + variable
+mensaje +=' Terminamos'
+#print(mensaje)
+
+# Usamos la clase help para ayudao o documentación (built-int)
+help(math.isnan)
