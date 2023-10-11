8ed945b9756e4dbf34423ad878dee378f1110ed4
nicolepenalbe1 <112666058+nicolepenalbe1@users.noreply.github.com>
2023-10-10 22:45:31 -0300

    subiendo clase 5 y 6

---
+++ LaboratorioIV/Python/Clase 06/mi_clase.py
@@ -0,0 +1,23 @@
+
+class MiClase:
+    '''
+    Este es un ejemplo de documentación
+     de esta clase.
+     es solo saber que aquí esta Miclase
+    '''
+
+    def __int__(self):
+        '''
+        Aqui esta el metodo dunder init
+        Este método es el de inicio
+        para esta clase
+        '''
+    def mi_metodo(self, param1, param2):
+        '''
+
+        Este es el método es creado por mi que recibe parámetros
+        detecto los parámetros automaticamente
+        :param param1: Este es el parámetro número 1
+        :param param2: Este es el parámetro número 2
+        :return: Este es el valor de retorno
+        '''
---
+++ LaboratorioIV/Python/Clase 06/tipo_str.py
@@ -0,0 +1,15 @@
+import math
+#profundizando en el tipo String
+#Concatenación automaticas en python
+
+variable = ' Adios'
+mensaje = 'Hola' + ' Alumnos' + variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+#Usamos la clase help para ayuda o documentación (built-in)
+help(math.isnan)
+help(str)
+help(str.capitalize)
+
+
