9a63d3a766aea2ac5fd4920c703fc23d7f26cb0c
Mayra Medina <111921764+Maynh@users.noreply.github.com>
2023-10-14 00:19:54 -0300

    subiendo

---
+++ lección01/doc_str.py
@@ -0,0 +1,4 @@
+from mi_clsse import MiClase
+
+#help(MiClase)
+print(MiClase.__doc__)
---
+++ lección01/mi_clsse.py
@@ -0,0 +1,23 @@
+
+
+class MiClase:
+    '''
+    Este es un ejemplo de la d0cumentación, de esta clase
+    <Es s´lo para saber que aqui besta mi clase
+    '''
+
+    def __init__(self):
+        '''
+        Aqui tambin vamos a poner documentacón
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi, que recibe parametros
+        Detecto los parametros automaticamente
+        :param param1: Este es el parametro número 1
+        :param param2: este es el parametro número 2
+        :return: Este es el valor de retorno
+        '''
+
+
